976becfd3d69678565d6de85f91c01f63bc43ce7
Alex Nachasic <nachasic@gmail.com>
2019-12-02 00:46:43 +0200

    Make first two tests pass

--- src/store/datastore.ts
+++ src/store/datastore.ts
@@ -5,8 +5,32 @@
  * - change data;
  * - report changes
  */
+import { StoreNotification, StoreOperation, NickNameMap } from './types';
 
+export class DataStore {
+    static store: DataStore;
+    private map: NickNameMap;
 
- export class DataStore {
-     
- }
+    constructor () {
+        if (DataStore.store) {
+            return DataStore.store;
+        }
+
+        this.map = new Map();
+        DataStore.store = this;
+
+        return this;
+    }
+
+    public destroy () {
+        this.map = undefined;
+    }
+
+    public report () {
+
+        console.log(this.map);
+        return Array.from(this.map).reduce(
+            (acc, [name, nick]) => Object.assign(acc, { [name]: nick }),
+            {})
+    }
+}
